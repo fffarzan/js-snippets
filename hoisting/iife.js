4ca7dcf4aba3f15470911400c432be3384748d9e
Farzan Nedaei <47377731+fffarzan@users.noreply.github.com>
2025-07-25 23:05:56 +0330

    Add hoisitng iife snippet

---
+++ hoisting/iife.js
@@ -0,0 +1,8 @@
+// Q1: var and iife (blubank)
+
+// var name = 'blubank';
+// (function () {
+//   console.log(name + ' but lovely');
+//   var name = 'blujunior';
+//   console.log(name + ' but for kids');
+// })();
